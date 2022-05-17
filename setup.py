a9033757b96a39aaf47fc42ed3e6a1b418964593
Nischal <nneupane1@gmail.com>
2022-05-17 02:48:39 +0200

    Setup Done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a Wine Quality package with MLOPs",
+    author="nneupane1",
+    packages=find_packages(),
+    license="MIT"
+)
